bf672c2a0f36729bcc6b4c633d53c7127a846181
Shailesh Vekariya <shaileshvekariya11@gmail.com>
2020-05-19 15:33:23 +0530

    Navbar Ready For Setup All Router And All

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AddComponent } from './work/add/add.component';
+import { RemoveComponent } from './work/remove/remove.component';
+import { CompetedComponent } from './work/competed/competed.component';
+import { ContactComponent } from './contact/contact.component';
+import { TodayComponent } from './today/today.component';
+import { LoginComponent } from './user/login/login.component';
+import { SigninComponent } from './user/signin/signin.component';
+import { ProfileComponent } from './user/profile/profile.component';
+
+const routes:Routes=[
+  {path:'home',component:HomeComponent},
+  {path:'today',component:TodayComponent},
+  {path:'work',children:[
+    {path:'add',component:AddComponent},
+    {path:'remove',component:RemoveComponent},
+    {path:'complete',component:CompetedComponent}
+  ]},
+  {path:'contact',component:ContactComponent},
+  {path:'user',children:[
+    {path:'login',component:LoginComponent},
+    {path:'signin',component:SigninComponent},
+    {path:'profile',component:ProfileComponent}
+  ]}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AddComponent,
+    RemoveComponent,
+    CompetedComponent,
+    ContactComponent,
+    TodayComponent,
+    LoginComponent,
+    SigninComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/work/competed/competed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-competed',
+  templateUrl: './competed.component.html',
+  styleUrls: ['./competed.component.css']
+})
+export class CompetedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
